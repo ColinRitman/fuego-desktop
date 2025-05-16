c5478d51200601edcb732ef7b720c96f73807d72
ᗧ···ᗣ···ᗣ <54386876+ColinRitman@users.noreply.github.com>
2025-05-16 12:18:53 -0400

    Update main.cpp
    try ordering swap for log file issue

--- src/main.cpp
+++ src/main.cpp
@@ -89,13 +89,14 @@ int main(int argc, char* argv[])
   Q_UNUSED(cmdLineParseResult)
 #endif
 
-  LoggerAdapter::instance().init();
-
   QString dataDirPath = Settings::instance().getDataDir().absolutePath();
   if (!QDir().exists(dataDirPath))
   {
     QDir().mkpath(dataDirPath);
   }
+
+
+  LoggerAdapter::instance().init();
 
   QLockFile lockFile(Settings::instance().getDataDir().absoluteFilePath(
       QApplication::applicationName() + ".lock"));
